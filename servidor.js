5db60f56cd4b77c9b8a2d7688b1d2229ced4f30e
Tarcísio Luiz Harres de Almeida <20221PF.INF_I0007@passofundo.ifsul.local>
2024-08-13 11:45:31 -0300

    a

--- servidor.js
+++ servidor.js
@@ -2,26 +2,15 @@ var express = require('express'); // requisita a biblioteca para a criacao dos s
 var pg = require("pg"); // requisita a biblioteca pg para a comunicacao com o banco de dados.
 
 var sw = express(); // iniciliaza uma variavel chamada app que possitilitará a criação dos serviços e rotas.
-<<<<<<< HEAD
 sw.use(express.json());//padrao de mensagens em json.
 //permitir o recebimento de qualquer origem, aceitar informações no cabeçalho e permitir o métodos get e post
 sw.use(function (req, res, next) {
-=======
-
-sw.use(express.json());//padrao de mensagens em json.
-//permitir o recebimento de qualquer origem, aceitar informações no cabeçalho e permitir o métodos get e post
-sw.use(function(req, res, next) {
->>>>>>> 51a029aa6a7be840e243ce1caedfa9f7f2d0e24b
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET,POST');
     next();
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 51a029aa6a7be840e243ce1caedfa9f7f2d0e24b
 const config = {
     host: 'localhost',
     user: 'postgres',
@@ -35,7 +24,6 @@ const postgres = new pg.Pool(config);
 
 //definicao do primeiro serviço web.
 sw.get('/', (req, res) => {
-<<<<<<< HEAD
     res.send('Hello darkness my old friend.  #####');
 })
 
@@ -122,49 +110,9 @@ sw.get('/listjogadores', function (req, res, next) {
                 }
             });
         }
-=======
-    res.send('Hello, world! meu primeiro teste.  #####');
-})
-
-sw.get('/teste', (req, res) => {
-    res.send('i am all of me');
-});
-
-sw.get('/listendereco', function (req, res, next) {
-    
-    postgres.connect(function(err,client,done) {
-
-       if(err){
-
-           console.log("Nao conseguiu acessar o  BD "+ err);
-           res.status(400).send('{'+err+'}');
-       }else{            
-
-            var q ='select * from tb_endereco';            
-    
-            client.query(q,function(err,result) {
-                done(); // closing the connection;
-                if(err){
-                    console.log('retornou 400 no listendereco');
-                    console.log(err);
-                    
-                    res.status(400).send('{'+err+'}');
-                }else{
-
-                    //console.log('retornou 201 no /listendereco');
-                    res.status(201).send(result.rows);
-                }           
-            });
-       }       
->>>>>>> 51a029aa6a7be840e243ce1caedfa9f7f2d0e24b
     });
 });
 
 sw.listen(4000, function () {
-<<<<<<< HEAD
-    console.log('Server tá na Porta 4000');
+    console.log('Server tá rodando meu chapa.. na Porta 4000');
 });
-=======
-    console.log('Server is running.. on Port 4000');
-});
->>>>>>> 51a029aa6a7be840e243ce1caedfa9f7f2d0e24b
